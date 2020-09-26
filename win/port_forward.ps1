24dac6082398fce22f4acd07b241db36e82054ca
Zhangyi Hu <hu.zhangyi@gmail.com>
2020-09-26 16:18:43 -0400

    Update port_forward.ps1

--- win/port_forward.ps1
+++ win/port_forward.ps1
@@ -1,41 +1,39 @@
 # set port forwarding to wsl2
-# this script need Admin right to run
-# need to change excecution policy first by running:
+# need to change excecution policy first by running
 # Set-ExecutionPolicy RemoteSigned
 
-$remoteport = bash.exe -c "ifconfig eth0 | grep 'inet '"
-$found = $remoteport -match '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}';
+# all the ports you want to forward
+$ports=@(2222,2223,80,443,2376);
+# allow inbound traffic from this remote address
+$remoteAddr='192.168.1.160';
+
+# get the ip address of wsl2 as forward destination
+$ipinfo = bash.exe -c "ifconfig eth0 | grep 'inet '"
+$found = $ipinfo -match '\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}';
 
 if( $found ){
-  $remoteport = $matches[0];
-  echo "will forward ports to $remoteport";
+  $forwardAddr = $matches[0];
+  echo "will forward ports to $forwardAddr";
 } else{
   echo "The Script Exited, the ip address of WSL 2 cannot be found";
   exit;
 }
 
-#[Ports]
 
-#All the ports you want to forward separated by coma
-$ports=@(2222,80,443);
-
-
-#[Static ip]
-#You can change the addr to your ip config to listen to a specific address
-$addr='0.0.0.0';
-$ports_a = $ports -join ",";
-
-
-#Remove Firewall Exception Rules
+# remove Firewall Exception Rules
 iex "Remove-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' ";
 
-#adding Exception Rules for inbound and outbound Rules
-iex "New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Outbound -LocalPort $ports_a -Action Allow -Protocol TCP";
-iex "New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Inbound -LocalPort $ports_a -Action Allow -Protocol TCP";
+# add rules for inbound traffic
+$ports_a = $ports -join ",";
+iex "New-NetFireWallRule -DisplayName 'WSL 2 Firewall Unlock' -Direction Inbound -RemoteAddress $remoteAddr  -LocalPort $ports_a -Action Allow -Protocol TCP";
+
+
+# add forward rules
+$all='0.0.0.0';
 
 for( $i = 0; $i -lt $ports.length; $i++ ){
   $port = $ports[$i];
   echo "setting port-forarding at port $port";
-  iex "netsh interface portproxy delete v4tov4 listenport=$port listenaddress=$addr";
-  iex "netsh interface portproxy add v4tov4 listenport=$port listenaddress=$addr connectport=$port connectaddress=$remoteport";
+  iex "netsh interface portproxy delete v4tov4 listenport=$port listenaddress=$all";
+  iex "netsh interface portproxy add v4tov4 listenport=$port listenaddress=$all connectport=$port connectaddress=$forwardAddr";
 }
